e9c6dac7ba84bf318f774ac396a7f303886e7dde
Neena <junkemail273@gmail.com>
2013-08-06 10:10:48 +0100

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HideAndStreet::Application.config.secret_key_base = '3c11ba85c8e71feb839e1a1a1a229df204c80f7bdee20bfd1187dfdb9eaeb9c222ac2eb7267641889d34f4ac64eca69ddb04e2ffae0d4be7bf0c5151ae7250b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HideAndStreet::Application.config.session_store :cookie_store, key: '_hide-and-street_session'
