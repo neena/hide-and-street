c2d43ff201553e95c9ca994d4eda202773572805
Neena <junkemail273@gmail.com>
2013-08-08 22:31:25 +0100

    Fixed the random chooser error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 	def show
-		if params[:id].to_i != 0
+		@finder = params[:id].to_i
+		if @finder != 0
 			@user = User.find(params[:id])
 		else 
 			@user = User.find_by_username(params[:id])
 		end
-		@endpoint = @user.challenges.offset(rand(Challenge.count)).first
+		@endpoint = @user.challenges.offset(1 + rand(Challenge.count - 1)).first
 	end
 end
