2b2f2ba238d2fc8f7c1bbd11d6f8e3382948e0a4
Neena <junkemail273@gmail.com>
2013-08-09 08:33:43 +0100

    Small fixes and tidying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 	def show
-		@finder = params[:id].to_i
-		if @finder != 0
+		if params[:id].to_i != 0
 			@user = User.find(params[:id])
 		else 
 			@user = User.find_by_username(params[:id])
 		end
-		@endpoint = @user.challenges.offset(rand(@user.challenges.count)).first
+		if @user.challenges.count > 0
+			@endpoint = @user.challenges.offset(rand(@user.challenges.count)).first
+		else
+			@endpoint = Challenge.first
 	end
 end
